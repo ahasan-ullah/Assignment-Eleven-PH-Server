291c08f33f6f26f23e372cb4a6a5d8636370fc1f
Ahasan Ullah <ahasanullah451@gmail.com>
2024-12-25 15:54:34 +0600

    all foods api added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors=require('cors');
 require('dotenv').config();
 const jwt=require('jsonwebtoken');
 const cookieparser=require('cookie-parser');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app=express();
 const port=process.env.PORT || 5000;
@@ -14,6 +15,53 @@ app.use(cookieparser());
 app.get('/',(req,res)=>{
   res.send('food is ready');
 })
+
+
+
+//mongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n7txs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    //Database
+    const allFoods=client.db('hungryNakiDB').collection('foods');
+
+    // allfoods
+    app.get('/foods',async(req,res)=>{
+      const cursor=allFoods.find();
+      const result=await cursor.toArray();
+      res.send(result);
+    })
+
+
+
+
+
+
+
+
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port,()=>{
   console.log(`food is cooking at ${port}`);
